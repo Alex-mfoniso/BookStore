ffa768162f49fad99dd3d3a388b6e59d3268da78
Alex-mfoniso <alexandermfoniso18@gmail.com>
2025-06-03 16:28:54 +0100

    mofe why

--- src/pages/Books/Composite.jsx
+++ src/pages/Books/Composite.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { List, Typography, Card, message, Spin } from "antd";
+import Compoid from "./Compoid";
 
 
 
@@ -84,7 +85,7 @@ const Composite = () => {
           )}
         />
       )}
-  
+ <Compoid/>
     </div>
   );
 };
